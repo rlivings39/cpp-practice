f296353a70f7a5405eb0d86efdc43be59f6312d7
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-05-05 09:25:51 -0400

    Basic timing code

--- matrix_main.cpp
+++ matrix_main.cpp
@@ -1,15 +1,27 @@
 #include "matrix_ops.hpp"
+#include <chrono>
 #include <iostream>
 #include <random>
 
-int main (int argc, char* argv[]) {
-    constexpr size_t n = 102;
-    std::vector<double> a(n*n);
-    for (size_t k = 0; k < a.size(); ++k) {
-        a[k] = k;
-    }
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage " << argv[0] << " num_rows\n";
+    return 1;
+  }
+  std::size_t n = std::stoll(argv[1]);
+  std::vector<double> a(n * n);
+  for (size_t k = 0; k < a.size(); ++k) {
+    a[k] = k;
+  }
 
-    auto res = matrix_multiply(a, n, n, a, n, n);
+  auto start = std::chrono::steady_clock::now();
+  auto res = matrix_multiply(a, n, n, a, n, n);
+  auto end = std::chrono::steady_clock::now();
 
-    return res[12] == 0;
+  std::cout << "Elapsed time for size (" << n << "," << n << "): "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end -
+                                                                     start)
+            << ".\n";
+
+  return res[12] == 0;
 }
