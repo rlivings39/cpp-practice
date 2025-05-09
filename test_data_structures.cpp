494a9dec3f6158a05c39fa4eb3e352bffa198820
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-05-09 06:46:51 -0400

    Simple list class and add actions

---
+++ test_data_structures.cpp
@@ -0,0 +1,32 @@
+
+#include <algorithm>
+#include <array>
+#include <list>
+#include <initializer_list>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <vector>
+#include <gtest/gtest.h>
+#include "data_structures.hpp"
+
+TEST(DataStructures, List) {
+    std::vector<int> data{1,2,7,12};
+    ry::list<int> l(data);
+    ASSERT_EQ(l.size(), 4);
+    auto b = l.begin();
+    ASSERT_EQ(*b, 1);
+    decltype(l)::size_type i{0};
+    std::for_each(l.begin(), l.end(), [&i, &data](int val) {
+        ASSERT_EQ(val, data[i]);
+        ++i;
+    });
+
+    std::ostringstream oss;
+    oss << l;
+
+    auto s = oss.str();
+    ASSERT_EQ(s, "[1,2,7,12]");
+}
+
+
