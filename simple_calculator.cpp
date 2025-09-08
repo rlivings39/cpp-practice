a8fef698e815ce4aa163b17cfc038f38756b90b5
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-09-08 19:44:21 -0400

    Basic skeleton

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -6,8 +6,47 @@ Calculator like thing that handles expressions of the form:
 for any integer constants and identifiers. Only +/- are supported.
 */
 
+#include <cctype>
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <unordered_map>
+
+struct calc_err : public std::runtime_error {
+  calc_err(const std::string &msg) : std::runtime_error(msg) {}
+};
+
+std::string simplify(const std::string &input) {
+  std::unordered_map<std::string, int> var_table;
+
+  std::size_t idx{0};
+  int sign{1};
+  while (idx < input.size()) {
+    auto c = input[idx];
+    if (std::isspace(c)) {
+      ++idx;
+      continue;
+    }
+    // Start lexing and parsing
+    if (c == '+') {
+      sign = 1;
+    } else if (c == '-') {
+      sign = -1;
+    } else if (std::isdigit(c)) {
+      // Parse number
+    } else if (std::isalpha(c)) {
+      // Parse identifier
+    } else {
+      std::string msg(idx, ' ');
+      msg += "^";
+      std::string err = std::string("Failed to parse '") + c + "':\n\n" +
+                        input + "\n" + msg + "\n";
+      throw calc_err(err);
+    }
+    ++idx;
+  }
+  return input;
+}
 
 int main(int argc, char *argv[]) {
   std::string prompt;
@@ -20,6 +59,12 @@ int main(int argc, char *argv[]) {
     // TODO trim
     if (prompt == "quit") {
       break;
+    }
+    try {
+      auto result = simplify(prompt);
+      std::cout << "  " << result << "\n";
+    } catch (calc_err &e) {
+      std::cerr << e.what();
     }
   }
   return 0;
